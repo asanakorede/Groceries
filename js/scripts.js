4691d3963d89d760b50432a439c137f5fb34df19
Asana Korede <asanakorededaniel@gmail.com>
2024-01-15 03:17:51 -0800

    add networks to labels and inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
+
     $("#formOne").submit(function (event) {
         const provisionsInput = $("input#provisions").val();
         const confectionaryInput = $("input#confectionary").val();
         const chocolatesInput = $("input#chocolates").val();
-        const exclamationInput = $("input#exclamation").val();
-        const verbInput = $("input#verb").val();
-        const nounInput = $("input#noun").val();
+        const cookiesInput = $("input#cookies").val();
+        const drinksInput = $("input#drinks").val();
+        const foodstuffInput = $("input#foodstuff").val();
 
-        $(".person1").prepend(person1Input);
-        $(".person2").prepend(person2Input);
-        $(".animal").prepend(animalInput);
-        $(".exclamation").prepend(exclamationInput);
-        $(".verb").prepend(verbInput);
-        $(".noun").prepend(nounInput);
+        $(".provisions").prepend(provisionsInput);
+        $(".confectionary").prepend(confectionaryInput);
+        $(".chocolates").prepend(chocolatesInput);
+        $(".cookies").prepend(cookiesInput);
+        $(".drinks").prepend(drinksInput);
+        $(".foodstuff").prepend(foodstuffInput);
 
-        $("#story").show();
+        const orders = [provisionsInput, confectionaryInput, chocolatesInput, cookiesInput, drinksInput, foodstuffInput]
+        orders = orders.sort();
+        const newOrders = [];
+        orders.forEach(function (element) {
+            newOrders.push(element.toUperCase());
+        })
+
+        console.log(newOrders);
+
+        $("#order").show();
+        $("#formOne").hide();
 
         event.preventDefault();
     })
